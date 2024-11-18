06dd8c6bd497aaeaf99c0444a6fdf3a7b5e8c319
Pak-Man926 <davidkamau926@gmail.com>
2024-11-18 15:48:14 +0300

    Third Commit

---
+++ app/src/main/java/user/app/touristapp/TouristSpot.java
@@ -0,0 +1,30 @@
+package user.app.touristapp;
+
+public class TouristSpot
+{
+    private String Name;
+    private String Description;
+    private int ImageResId;
+
+    public TouristSpot(String Name, String Description, int ImageResId)
+    {
+        this.Name = name;
+        this.Description = description;
+        this.ImageResId = imageResId;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public int ImageResId()
+    {
+        return imagesResId;
+    }
+}
